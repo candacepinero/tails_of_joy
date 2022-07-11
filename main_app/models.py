090c40760b3b7fea49c35bc78e4ca193e75f3a9d
candace pinero <candcae.pinero@gmail.com>
2022-07-11 12:48:53 -0400

    full crud and user auth

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,19 @@
+from django.urls import reverse
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Pet(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=50)
+    lives_in = models.CharField(max_length=100)
+    personality = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'pet_id': self.id})
+    
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Pet
 
-# Create your views here.
+# Home view
 def home(request):
     return render(request, 'home.html')
+
+# index view
+@login_required
+def pets_index(request):
+    pets = Pet.objects.filter(user=request.user)
+    return render(request, 'pets/index.html', {'pets': pets})
+
+# detail view
+@login_required
+def pets_detail(request, pet_id):
+    pet = Pet.objects.get(id=pet_id)
+    return render(request, 'pets/detail.html', {'pet': pet})
+
+# sign up view
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        error_message = 'Invalid sign up - try again'
+        form = UserCreationForm()
+        context = {'form': form, 'error_message': error_message}
+        return render(request, 'registration/signup.html', context)
+
+# create view
+class PetCreate(LoginRequiredMixin,  CreateView):
+    model = Pet
+    fields = ['name', 'age', 'gender', 'lives_in', 'personality']
+    success_url = '/pets/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+# update view
+class PetUpdate(LoginRequiredMixin, UpdateView):
+    model = Pet
+    fields = ['age', 'lives_in',  'personality']
+
+class PetDelete(LoginRequiredMixin, DeleteView):
+    model = Pet
+    success_url = '/pets/'
